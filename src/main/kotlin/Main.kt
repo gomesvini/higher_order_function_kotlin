6c44aa38f025f2fecdff16edecf59ab5fd022518
Vinicius Gomes <pain-nici2011@live.com>
2024-01-13 17:53:08 -0300

    examples about higher order function

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun calculate (x: Int, y: Int, operation: (Int,Int) -> Int) : Int {
+    return operation(x,y)
+}
+
+fun sum(x: Int, y: Int) = x + y
+fun main() {
+    val sumResult = calculate(4,5, :: sum)
+    val multResult = calculate(4,5) { a, b -> a * b }
+    println("sumResult $sumResult, multResult $multResult")
+}
